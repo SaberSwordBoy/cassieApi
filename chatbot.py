774d1b3a5a538b527db9f255cc13c05b77ab4d74
SaberSwordBoy <saberfilmsproductions@gmail.com>
2022-03-27 21:57:24 +0000

    changes

--- chatbot.py
+++ chatbot.py
@@ -6,7 +6,7 @@ def help_function():
 mappings = {"help": help_function}
 assistant = neuralintents.GenericAssistant('data/intents.json', model_name="data/model", intent_methods=mappings)
 
-#assistant.train_model()
-#assistant.save_model()
+assistant.train_model()
+assistant.save_model()
 
-assistant.load_model()
+#assistant.load_model()
--- server.py
+++ server.py
@@ -3,6 +3,9 @@ from flask_restful import Api, Resource, reqparse
 import json
 import random
 import chatbot
+from rich import print
+
+DEBUG = False
 
 print("Starting up...")
 
@@ -11,11 +14,10 @@ api = Api(app)
 
 FILTER_CHARS = ["!", ",", '"', '"', "?", ".", "@", "#", "$", "%", "^", "&", "*", "(", ")",]
 
-
 def remove_unwanted_chars(string):
     """Remove Filter Chars from the input string"""
-    for i in string:
-        if i in FILTER_CHARS:
+    for i in string: 
+        if i in FILTER_CHARS: 
             string = string.replace(i, "")
     return string
 
@@ -28,19 +30,20 @@ class GetResponse(Resource):
     Page for getting responses to input
     """
     def post(self):
+        data_resp = {'data': {}}
         parser = reqparse.RequestParser()  # initialize
         parser.add_argument('input', required=True)  # add args
         args = parser.parse_args()  # parse arguments to dictionary
+        if args['input'] == "" or args['input'] == " ": return "ERROR: No Input", 400 # return No Input with 400 ERROR if there is no input
+
         response = get_ai_response(args['input'])
 
-        if response == "":
-            return "No input", 400
+        if response == "help": # check for special responses
+            data_resp['data']['func'] = "assistant_help"
 
-        print(f"[#] Responded to query '{args['input']}' with {response}") # Print out query/response
-        
-        return {'data': {
-            "response": response
-        }}, 200  # return data with 200 OK
+        data_resp['data']['response'] = response
+        print(f"[green][✅] Responding to query '{args['input']}' with {response}, data: {data_resp}[/green]") # Print out query/response
+        return data_resp, 200  # return data with 200 OK
 
     def get(self):
         return 'How did you GET here? (You should submit a POST request)', 200 # return data with 200 OK
@@ -56,7 +59,6 @@ class Index(Resource):
 api.add_resource(Index, "/")
 api.add_resource(GetResponse, '/getresponse')
 
-
 # Run the program
 if __name__ == '__main__':
-    app.run(host="", port=80) # host needs to be server IP or will not work. Port 80 works best. 
+    app.run(host="45.33.77.180", port=80) # host needs to be server IP or will not work. Port 80 works best. 
