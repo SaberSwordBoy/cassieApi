677afdeaa05d0b24c8c49633b536793ff1b8fbae
SaberSwordBoy <saberfilmsproductions@gmail.com>
2022-03-27 18:40:31 +0000

    Update Client

--- client.py
+++ client.py
@@ -1,7 +1,27 @@
 import requests
 
-url = "http://localhost:5000/getresponse"
+# Urls
+BASE_URL = 'http://45.33.77.180/'
+PORT = 80 # Not needed currently
+GETRESP_URL = 'getresponse'
 
+exit_vars = ["q", "exit", "quit"] # If the user types anything in this list, the program will quit. 
+
+# Banner
+print("-"*50)
+print("Welcome to Cassandra!")
+print("-"*50)
+print("Type a message and hit enter. Your message gets sent to the Cassie servers and processed using AI.\n"\
+    "The first message might take a few seconds longer than the rest.\n\n")
+# Main Loop
+
+print("Enter text to say to Cassandra")
 while True:
-    data = {"input": input(">>> ")}
-    print(requests.post(url, data=data).json()['data']['response'])
+    input_text = input(">>> ")
+    
+    if input_text in exit_vars:
+        print("Exiting!")
+        break
+
+    print("You: " + input_text)
+    print("Cassie: " + requests.post(BASE_URL + GETRESP_URL, data={"input": input_text}).json()['data']['response'])
--- server.py
+++ server.py
@@ -11,30 +11,52 @@ api = Api(app)
 
 FILTER_CHARS = ["!", ",", '"', '"', "?", ".", "@", "#", "$", "%", "^", "&", "*", "(", ")",]
 
+
 def remove_unwanted_chars(string):
+    """Remove Filter Chars from the input string"""
     for i in string:
         if i in FILTER_CHARS:
             string = string.replace(i, "")
     return string
 
 def get_ai_response(input_message):
+    """Request a response to input from the chatbot"""
     return chatbot.assistant.request(input_message)
 
 class GetResponse(Resource):
+    """
+    Page for getting responses to input
+    """
     def post(self):
         parser = reqparse.RequestParser()  # initialize
         parser.add_argument('input', required=True)  # add args
         args = parser.parse_args()  # parse arguments to dictionary
         response = get_ai_response(args['input'])
 
-        print(f"[#] Responded to query '{args['input']}' with {response}")
+        if response == "":
+            return "No input", 400
+
+        print(f"[#] Responded to query '{args['input']}' with {response}") # Print out query/response
+        
         return {'data': {
             "response": response
         }}, 200  # return data with 200 OK
 
     def get(self):
-        return 'Submit a post request', 200 # return data with 200 OK
+        return 'How did you GET here? (You should submit a POST request)', 200 # return data with 200 OK
 
+class Index(Resource):
+    """
+    Default page, nothing really here. Maybe add HTML?
+    """
+    def get(self):
+        return "Welcome to Cassandra! To get started, submit a post request to /getresponse with some input.", 200
+
+# Add the pages
+api.add_resource(Index, "/")
 api.add_resource(GetResponse, '/getresponse')
+
+
+# Run the program
 if __name__ == '__main__':
-    app.run()
+    app.run(host="45.33.77.180", port=80) # host needs to be server IP or will not work. Port 80 works best. 
